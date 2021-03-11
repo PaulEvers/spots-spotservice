b01be924ae56414a65c23bf7d6b4e3d46b6426f8
Paul <paulevers1311@gmail.com>
2021-03-11 14:04:59 +0100

    Properties

--- src/main/java/nl/paulevers/spotservice/SpotserviceApplication.java
+++ src/main/java/nl/paulevers/spotservice/SpotserviceApplication.java
@@ -2,6 +2,7 @@ package nl.paulevers.spotservice;
 
 import nl.paulevers.spotservice.entities.Customer;
 import nl.paulevers.spotservice.entities.CustomerRepository;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.slf4j.Logger;
@@ -14,32 +15,19 @@ import java.util.List;
 
 @SpringBootApplication
 public class SpotserviceApplication {
-	private final Logger logger = LoggerFactory.getLogger(SpotserviceApplication.class);
 
-	@Autowired private CustomerRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpotserviceApplication.class, args);
 	}
 
-	@EventListener(ApplicationReadyEvent.class)
-	public void runAfterStartup() {
-		queryAllCustomers();
-		createCustomer();
-		queryAllCustomers();
-	}
+//	@EventListener(ApplicationReadyEvent.class)
+//	public void runAfterStartup() {
+//		queryAllCustomers();
+//		createCustomer();
+//		queryAllCustomers();
+//	}
 
-	private void createCustomer() {
-		Customer newCustomer = new Customer();
-		newCustomer.setFirstName("John");
-		newCustomer.setLastName("Doe");
-		logger.info("Saving new customer...");
-		this.repository.save(newCustomer);
-	}
 
-	private void queryAllCustomers() {
-		List<Customer> allCustomers = this.repository.findAll();
-		logger.info("Number of customers: " + allCustomers.size());
-	}
 
 }
