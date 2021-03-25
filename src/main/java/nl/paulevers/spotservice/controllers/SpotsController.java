57384b26c5414e0274f7092863296b94cd460230
Paul <paulevers1311@gmail.com>
2021-03-25 12:51:59 +0100

    Eurake client

--- src/main/java/nl/paulevers/spotservice/controllers/SpotsController.java
+++ src/main/java/nl/paulevers/spotservice/controllers/SpotsController.java
@@ -1,7 +1,11 @@
 package nl.paulevers.spotservice.controllers;
 
+import com.netflix.discovery.EurekaClient;
 import nl.paulevers.spotservice.entities.Spot;
 import nl.paulevers.spotservice.repositories.ISpotsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +14,10 @@ import java.util.List;
 
 @RestController
 public class SpotsController {
+
+    @Autowired
+    @Lazy
+    private EurekaClient eurekaClient;
 
     ISpotsRepository spotsRepository;
 
