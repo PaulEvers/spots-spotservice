07493bf12c9027cb6afab18822ac3cb703285bab
Paul <paulevers1311@gmail.com>
2021-06-08 14:56:58 +0200

    Unit tests

---
+++ src/test/java/nl/paulevers/spotservice/SpotTests.java
@@ -0,0 +1,36 @@
+package nl.paulevers.spotservice;
+
+import nl.paulevers.spotservice.entities.Coordinates;
+import nl.paulevers.spotservice.entities.Spot;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class SpotTests {
+
+    Spot spot = new Spot();
+
+    @Test
+    void incrementLike() {
+        assertThat(spot.getLikes()).isEqualTo(0);
+        spot.incrementLike();
+        assertThat(spot.getLikes()).isEqualTo(1);
+    }
+
+    @Test
+    void decrementLike() {
+        assertThat(spot.getLikes()).isEqualTo(0);
+        spot.incrementLike();
+        assertThat(spot.getLikes()).isEqualTo(1);
+        spot.decrementLike();
+        assertThat(spot.getLikes()).isEqualTo(0);
+    }
+
+    @Test
+    void coordinates() {
+        Coordinates coordinates = new Coordinates(3.0, 5.0);
+        AssertThat
+    }
+}
--- src/test/java/nl/paulevers/spotservice/SpotserviceApplicationTests.java
+++ src/test/java/nl/paulevers/spotservice/SpotserviceApplicationTests.java
@@ -1,13 +1,20 @@
 package nl.paulevers.spotservice;
 
+import nl.paulevers.spotservice.controllers.SpotsController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class SpotserviceApplicationTests {
+	@Autowired
+	private SpotsController controller;
 
 	@Test
 	void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
 
 }
